20ae067a98258b0faf0167a855fc8b86e5522da4
Chaimaa Lougsari <lougsari@outlook.com>
2022-02-21 16:10:47 +0100

    scrollView

--- App.tsx
+++ App.tsx
@@ -1,9 +1,13 @@
 
 import React from 'react';
 import LoginScreen from './src/screens/LoginScreen';
+import { TermsScreen } from './src/screens/TermsScreen';
+
 
 const App = () => {
-  return <LoginScreen />;
+  // return <LoginScreen />; 
+  return <TermsScreen />;
+  // return <WhatEverScreen />;
 };
 
 export default App;
