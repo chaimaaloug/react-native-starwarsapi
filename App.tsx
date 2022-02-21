3ad9272d2f2fd3adc840ebbc789f6e0bf9e11478
Chaimaa Lougsari <lougsari@outlook.com>
2022-02-21 23:57:39 +0100

    add images

--- App.tsx
+++ App.tsx
@@ -1,12 +1,15 @@
 
 import React from 'react';
 import LoginScreen from './src/screens/LoginScreen';
+import { StarshipFeedScreen } from './src/screens/StarshipFeedScreen';
 import { TermsScreen } from './src/screens/TermsScreen';
+
 
 
 const App = () => {
   // return <LoginScreen />; 
-  return <TermsScreen />;
+  // return <TermsScreen />;
+  return <StarshipFeedScreen />;
   // return <WhatEverScreen />;
 };
 
---
+++ src/screens/StarshipFeedScreen.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { SafeAreaView, StyleSheet, Text, StatusBar, View } from "react-native";
+
+import { default as data } from "../../api/data.json";
+
+export const StarshipFeedScreen = () => {
+  return (
+    <SafeAreaView style={styles.safeContainer}>
+      <View style={styles.container}>
+        <Text>{JSON.stringify(data.results)}</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  safeContainer: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
+  },
+  container: {
+    paddingHorizontal: 20,
+    marginTop: 20,
+  },
+});
